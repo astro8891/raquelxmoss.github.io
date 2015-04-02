50271b66a46339b067864fed248b4fe8692424c4
Raquel Moss <raquelxmoss@gmail.com>
2015-04-02 21:12:41 +1300

    colourful random circles

--- art_fuck_yeah/app/controllers/controller.js
+++ art_fuck_yeah/app/controllers/controller.js
@@ -4,18 +4,20 @@ function Controller() {
   }
   var view = new View();
   var box = new Model();
-  var size = 10;
+  var size = 5;
   var rgb1 = randomNumber();
   var rgb2 = randomNumber();
   var rgb3 = randomNumber();
+  var padding = 0;
 
   this.partyTime = function() {
     initialize();
     var cycle = setInterval(function() {
-      var newBox = box.generateBox(size, rgb1, rgb2, rgb3);
+      var newBox = box.generateBox(size, rgb1, rgb2, rgb3, padding);
       view.addBox(newBox);
       view.scrollDown();
-      size += 2;
+      size = randomNumber();
+      padding = randomNumber();
       rgb1 = randomNumber();
       rgb2 = randomNumber();
       rgb3 = randomNumber();
--- art_fuck_yeah/app/models/model.js
+++ art_fuck_yeah/app/models/model.js
@@ -1,11 +1,12 @@
 function Model() {
 }
 
-Model.prototype.generateBox = function (size, rgb1, rgb2, rgb3) {
+Model.prototype.generateBox = function (size, rgb1, rgb2, rgb3, padding) {
   var $div = $('<div></div>').css({
     'height' : size + 'px',
     'width' : size + 'px',
-    'border-color' : 'rgb('+rgb1+','+rgb2+','+rgb3+')'
+    'border-color' : 'rgb('+rgb1+','+rgb2+','+rgb3+')',
+    'padding': padding
   });
   return $div;
 }
